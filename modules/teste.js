716d6ae05f1c6ccefbcc9022a31cd0c8febbec02
LeslyNotter <notter.lesly@gmail.com>
2020-11-13 16:11:52 +0100

    primeiro commit

---
+++ modules/teste.js
@@ -0,0 +1,13 @@
+
+
+
+export default function teste(){
+
+const fruta= ['banana', 'maca', 'abacaxi']
+const legume= ['tomate', 'alface', 'cenoura']
+
+
+const novaLista=[...fruta, 'pizza', ...legume]
+
+console.log(novaLista)
+}
---
+++ script.js
@@ -0,0 +1,29 @@
+import initTabNav from'./modules/tabNav.js'
+import initAccordion from './modules/accordion.js'
+import initModal from './modules/modal.js'
+import initTooltip from './modules/tooltip.js'
+import initdropDownMenu from './modules/dropDownMenu.js'
+import initmenuMobile from './modules/menuMobile.js'
+import initanimaScroll from './modules/animaScroll.js'
+import initFuncionamento from './modules/funcionamento.js'
+import initfetchAnimais from './modules/fetchanimais.js'
+import initfetchBitcoins from './modules/bitcoinfetch.js'
+import teste from './modules/teste.js'
+
+initTabNav();
+initAccordion();
+initModal();
+initTooltip();
+initdropDownMenu();
+initmenuMobile();
+initanimaScroll();
+initFuncionamento();
+initfetchAnimais();
+initfetchBitcoins();
+teste();
+
+
+
+
+
+
